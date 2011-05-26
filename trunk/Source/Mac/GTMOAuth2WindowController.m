26762786bae99350bb2aea9ec1bd79143e086018
grobbins@google.com <grobbins@google.com@22754eea-fc93-e339-b032-c1f495b90c16>
2011-05-26 22:45:53 +0000

    Perform delayed invocations with NSRunLoopCommonModes so signing in works in modal dialogs

--- trunk/Source/Mac/GTMOAuth2WindowController.m
+++ trunk/Source/Mac/GTMOAuth2WindowController.m
@@ -143,7 +143,8 @@ const char *kKeychainAccountName = "OAuth";
     // start the asynchronous load of the sign-in web page
     [[self.webView mainFrame] performSelector:@selector(loadRequest:)
                                    withObject:self.initialRequest
-                                   afterDelay:0.01];
+                                   afterDelay:0.01
+                                      inModes:[NSArray arrayWithObject:NSRunLoopCommonModes]];
   } else {
     // clock date is invalid, so signing in would fail with an unhelpful error
     // from the server. Warn the user in an html string showing a watch icon,
@@ -276,7 +277,8 @@ const char *kKeychainAccountName = "OAuth";
   } else {
     [[self window] performSelector:@selector(close)
                         withObject:nil
-                        afterDelay:0.1];
+                        afterDelay:0.1
+                           inModes:[NSArray arrayWithObject:NSRunLoopCommonModes]];
   }
   isWindowShown_ = NO;
 }
